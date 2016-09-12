643df08320500764473f5ac92c59169ed46093a1
Matthew Wang <matthewang.wang@gmail.com>
2016-09-11 21:00:34 -0400

    fixes nav

--- navbar.php
+++ navbar.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-default">
   <a class="navbar-brand" href="index.php">OMUN Online Registration</a>
   <?php if (!(empty($_SESSION["school"]))){ ?>
-  <div class="navbar-right">
-    <a href="actions/logout.php">Logout</a>
-  </div>
+  <ul class="nav navbar-nav navbar-right">
+    <li><a href="actions/logout.php">Logout</a></li>
+  </ul>
   <?php }?>
 </nav>
