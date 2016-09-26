5cb87bf528016171fc7ca425b14380b7b167a9bc
Matthew Wang <matthewang.wang@gmail.com>
2016-09-26 13:18:41 -0400

    updates nav to include back to OMUN

--- navbar.php
+++ navbar.php
@@ -1,10 +1,12 @@
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">OMUN Online Registration</a>
-    <?php if (!(empty($_SESSION["school"]))){ ?>
     <ul class="nav navbar-nav navbar-right">
+      <?php if (!(empty($_SESSION["school"]))){ ?>
       <li><a href="actions/logout.php">Logout</a></li>
+      <?php } else{ ?>
+      <li><a href="https://omun.ca">Back to OMUN</a></li>
+      <?php } ?>
     </ul>
-    <?php }?>
   </div>
 </nav>
